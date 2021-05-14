afda257a337cfdd133e8862db14dedf74cb75b0c
HenBagle <ben.hagle@gmail.com>
2021-05-13 20:27:53 -0400

    Add MELE support, add .exe and registry detection options

--- AutoTOC/Program.cs
+++ AutoTOC/Program.cs
@@ -3,53 +3,106 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace AutoTOC
 {
     class Program
     {
+        enum MEGame
+        {
+            ME3,
+            LE1,
+            LE2,
+            LE3
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Detecting game...");
+            string gameDir;
+
             if (args.Length == 1)
             {
-                Console.WriteLine("Generating TOCs...");
-                string gameDir = args[0];
+                // Path is passed in, hopefully is game root directory or .exe
+                gameDir = args[0];
                 if (gameDir.EndsWith("\""))
                 {
                     gameDir = gameDir.Remove(gameDir.Length - 1);
                 }
-                string baseDir = Path.Combine(gameDir, @"BIOGame\");
-                string dlcDir = Path.Combine(baseDir, @"DLC\");
-                List<string> folders = (new DirectoryInfo(dlcDir)).GetDirectories().Select(d => d.FullName).ToList();
-                folders.Add(baseDir);
+                else if (gameDir.EndsWith(".exe"))
+                {
+                    gameDir = GetGamepathFromExe(gameDir);
+                }
+            }
+            else if (args.Length == 2 && args[0] == "-r")
+            {
+                try {
+                    MEGame game = (MEGame)Enum.Parse(typeof(MEGame), args[1], true);
 
-                Task.WhenAll(folders.Select(loc => TOCAsync(loc))).Wait();
-
-                Console.WriteLine("Done!");
+                    gameDir = GetGamepathFromRegistry(game);
+                    if(game != MEGame.ME3)
+                    {
+                        switch(game){
+                            case MEGame.LE1:
+                                gameDir = Path.Combine(gameDir, "ME1");
+                                break;
+                            case MEGame.LE2:
+                                gameDir = Path.Combine(gameDir, "ME2");
+                                break;
+                            case MEGame.LE3:
+                                gameDir = Path.Combine(gameDir, "ME3");
+                                break;
+                            default:
+                                throw new ArgumentException();
+                        }
+                    }
+                }
+                catch (ArgumentException e){
+                    Console.WriteLine("Not a supported Mass Effect game");
+                    return;
+                }
+                catch {
+                    Console.WriteLine("Unable to detect gamepath from registry");
+                    return;
+                }
             }
             else
             {
-                Console.WriteLine("Requires one argument: the install dir of ME3.");
-                Console.WriteLine("(eg. \"C:\\Program Files (x86)\\Origin Games\\Mass Effect 3\")");
+                Console.WriteLine("Requires one argument: the install dir or .exe of the game you're trying to TOC.");
+                Console.WriteLine("(eg. \"D:\\Origin Games\\Mass Effect Legendary Edition\\ME3\")");
+                Console.WriteLine("Detect game from registry with -r {game}. Options: ME3, LE1, LE2, LE3");
+                return;
             }
+
+            Console.WriteLine("Generating TOCs...");
+            GenerateTocFromGamedir(gameDir);
+            Console.WriteLine("Done!");
+        }
+
+        static void GenerateTocFromGamedir(string gameDir)
+        {
+            string baseDir = Path.Combine(gameDir, @"BIOGame\");
+            string dlcDir = Path.Combine(baseDir, @"DLC\");
+            List<string> folders = (new DirectoryInfo(dlcDir)).GetDirectories().Select(d => d.FullName).ToList();
+            folders.Add(baseDir);
+            Task.WhenAll(folders.Select(loc => TOCAsync(loc))).Wait();
         }
 
         static Task TOCAsync(string tocLoc)
         {
-            return Task.Run(() => prepareToCreateTOC(tocLoc));
+            return Task.Run(() => PrepareToCreateTOC(tocLoc));
         }
 
-        static void prepareToCreateTOC(string consoletocFile)
+        static void PrepareToCreateTOC(string consoletocFile)
         {
-            if (!consoletocFile.EndsWith("\\"))
-            {
-                consoletocFile = consoletocFile + "\\";
-            }
+            if (!consoletocFile.EndsWith("\\")) consoletocFile += "\\";
             List<string> files = GetFiles(consoletocFile);
-            if (files.Count != 0)
+
+            if (files.Count > 0)
             {
                 string t = files[0];
-                int n = t.IndexOf("DLC_");
+                int n = t.LastIndexOf("DLC_");
                 if (n > 0)
                 {
                     for (int i = 0; i < files.Count; i++)
@@ -61,7 +114,7 @@ namespace AutoTOC
                 }
                 else
                 {
-                    n = t.IndexOf("BIOGame");
+                    n = t.LastIndexOf("BIOGame");
                     if (n > 0)
                     {
                         for (int i = 0; i < files.Count; i++)
@@ -70,7 +123,7 @@ namespace AutoTOC
                 }
                 string pathbase;
                 string t3 = files[0];
-                int n2 = t3.IndexOf("BIOGame");
+                int n2 = t3.LastIndexOf("BIOGame");
                 if (n2 >= 0)
                 {
                     pathbase = Path.GetDirectoryName(Path.GetDirectoryName(consoletocFile)) + "\\";
@@ -125,7 +178,7 @@ namespace AutoTOC
         {
             List<string> res = new List<string>();
             string test = Path.GetFileName(Path.GetDirectoryName(basefolder));
-            string[] files = DirFiles(basefolder);
+            string[] files = GetTocableFiles(basefolder);
             res.AddRange(files);
             DirectoryInfo folder = new DirectoryInfo(basefolder);
             DirectoryInfo[] folders = folder.GetDirectories();
@@ -136,18 +189,53 @@ namespace AutoTOC
                 else
                     foreach (DirectoryInfo f in folders)
                         if (f.Name == "CookedPCConsole" || f.Name == "Movies" || f.Name == "Splash")
-                            res.AddRange(GetFiles(basefolder + f.Name + "\\"));
+                            res.AddRange(GetFiles(Path.Combine(basefolder, f.Name)));
+                        else if (f.Name == "Content")
+                            res.AddRange(GetFiles(Path.Combine(basefolder, f.Name, "Packages\\ISACT")));
+            
             return res;
         }
 
-        static string[] Pattern = { "*.pcc", "*.afc", "*.bik", "*.bin", "*.tlk", "*.txt", "*.cnd", "*.upk", "*.tfc" };
+        static string[] Pattern = { "*.pcc", "*.afc", "*.bik", "*.bin", "*.tlk", "*.txt", "*.cnd", "*.upk", "*.tfc", "*.isb" };
 
-        static string[] DirFiles(string path)
+        static string[] GetTocableFiles(string path)
         {
             List<string> res = new List<string>();
             foreach (string s in Pattern)
                 res.AddRange(Directory.GetFiles(path, s));
             return res.ToArray();
+        }
+
+        static string[] ValidExecutables = { "MassEffect1.exe", "MassEffect2.exe", "MassEffect3.exe" };
+
+        static string GetGamepathFromExe(string path)
+        {
+            if(path != null && ValidExecutables.Any((exe) => path.EndsWith(exe)))
+            {
+                return path.Substring(0, path.LastIndexOf("Binaries", StringComparison.OrdinalIgnoreCase));
+            }
+            else throw new ArgumentException("Executable file is not a supported Mass Effect game.");
+        }
+
+        static string GetGamepathFromRegistry(MEGame game)
+        {
+            if(game != MEGame.ME3)
+            {
+                // TODO
+                // Get LE path from registry
+                throw new NotImplementedException("LE registry key unknown");
+            }
+            else
+            {
+                // Get ME3 path from registry
+                string hkey32 = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\";
+                //string hkey64 = @"HKEY_LOCAL_MACHINE\SOFTWARE\";
+                string subkey = @"BioWare\Mass Effect 3";
+
+                string keyName = hkey32 + subkey;
+                return (string)Registry.GetValue(keyName, "Install Dir", null);
+            }
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # AutoTOC
-Standalone program to create the PCConsoleTOC.bin files for Mass Effect 3
+Standalone program to create the PCConsoleTOC.bin files for ME3 and Mass Effect Legendary Edition.
 
-Run from the command line, just pass it the root ME3 folder (eg. "C:\Program Files (x86)\Origin Games\Mass Effect 3"), and it will generate a TOC for the base game and all DLCs.
+Run from the command line, just pass it the root ME3 folder or .exe, and it will generate a TOC for the base game and all DLCs.
+eg. .\AutoTOC.exe "C:\Program Files (x86)\Origin Games\Mass Effect 3"
+
+You can also drag a game executable file directly onto AutoTOC in Windows Explorer to TOC.
+
+You can use the registry detection feature with the -r flag and the game you're trying to TOC.
+
+eg: .\AutoTOC.exe -r LE2
